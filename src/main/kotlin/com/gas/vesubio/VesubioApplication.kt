108f38672476111569aa3142d4c994ca4880a701
Paulina20lab <pazuero@est.ups.edu.ec>
2023-04-26 13:12:12 -0500

    First Commit
    Basic configurations

---
+++ src/main/kotlin/com/gas/vesubio/VesubioApplication.kt
@@ -0,0 +1,11 @@
+package com.gas.vesubio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VesubioApplication
+
+fun main(args: Array<String>) {
+	runApplication<VesubioApplication>(*args)
+}
