8c71332ebd50c6d00f6793752c33f9b35cf53ed4
Leonardo Crespo <lcrespos@est.ups.edu.ec>
2023-04-28 09:27:22 -0500

    CRUD valores de registro

---
+++ src/main/kotlin/com/gas/vesubio/Controller/Inf_ValoresController.kt
@@ -0,0 +1,43 @@
+package com.gas.vesubio.Controller
+
+import com.gas.vesubio.Model.Inf_Valores
+import com.gas.vesubio.Service.Inf_ValoresService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/info_valores")
+class Inf_ValoresController {
+    @Autowired
+    private val valoresService:Inf_ValoresService?=null
+    @GetMapping("/{id}")
+    fun findInfoValoresById(@PathVariable("var_id") var_id: Long): ResponseEntity<Inf_Valores?> {
+        val infoValores = valoresService!!.findValueById(var_id)
+
+        return if (infoValores != null) {
+                ResponseEntity.ok(infoValores)
+            } else {
+                ResponseEntity.notFound().build()
+            }
+    }
+    /*@PostMapping("/guardar")
+    fun saveInfoValores(@RequestBody infoValores: Inf_Valores): ResponseEntity<Inf_Valores> {
+        val savedInfoValores = valoresService!!.saveValues(infoValores)
+        return ResponseEntity.ok(savedInfoValores)
+    }*/
+    @GetMapping("/todos")
+    fun findAllInfoValores(): ResponseEntity<List<Inf_Valores>> {
+        val infoValoresList = valoresService!!.findAllValues()
+        return ResponseEntity.ok(infoValoresList)
+    }
+
+
+
+
+
+
+
+
+
+}
