29fc67e9285b0a008211cc1f5577c9df0d5828de
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-05-05 09:00:10 -0500

    CRUD valores de registro

--- src/main/kotlin/com/gas/vesubio/models/entity/RegisterDate.kt
+++ src/main/kotlin/com/gas/vesubio/models/entity/RegisterDate.kt
@@ -12,14 +12,26 @@ import javax.validation.constraints.Email
 import javax.validation.constraints.Size
 import javax.xml.crypto.Data
 
+/**
+ * Represents the date entity of register
+ *
+ * @property id  unique identifier of Register Date
+ * @property date Register Date.
+ */
 @Entity
 @Table(name = "register_dates")
 class RegisterDate: Serializable {
+    /**
+     * unique identifier of Register Date
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "date_id")
     val id: Long? = 0
 
+    /**
+     * Register Date.
+     */
     @Column(name = "date", nullable = false)
     val date: Date? = null
 
--- src/main/kotlin/com/gas/vesubio/models/entity/ValuesType.kt
+++ src/main/kotlin/com/gas/vesubio/models/entity/ValuesType.kt
@@ -8,15 +8,26 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
+/**
+ * Represents Value Type of these entity.
+ *
+ * @property id Unique identifier of Value Type
+ * @property description Description of Value Type
+ */
 @Entity
 @Table(name = "values_type")
 class ValuesType: Serializable {
-
+    /**
+     * Unique identifier of Value Type
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "values_tpe_id")
     val id: Long = 0
 
+    /**
+     * Description of Value Type
+     */
     @Column(name = "description", nullable = false)
     val description:String? = null
 
